1c4438d5924e3921447b2f10490fd4d40275bf4f
prachigupta10 <prachigu@yahoo.com>
2017-08-25 21:27:45 +0530

    Added comment to method add, added sub method

--- TestGitBranch/src/com/pg/Calculator.java
+++ TestGitBranch/src/com/pg/Calculator.java
@@ -2,8 +2,13 @@ package com.pg;
 
 /*This is class does basic operations */
 public class Calculator {
+	//This method adds two numbers
 	public  int add (int x, int y) {
 		return x+y;
+	}
+	
+	public int sub (intx, int y){
+		return x-y;
 	}
 
 }
